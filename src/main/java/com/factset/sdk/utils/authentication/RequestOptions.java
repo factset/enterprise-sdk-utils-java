427d33c359d48aaea3a083974692624c6c25f29b
Filip Andonie <47191006+Filip1x9@users.noreply.github.com>
2025-09-18 14:56:28 +0100

    chore: use correct version

--- src/main/java/com/factset/sdk/utils/authentication/RequestOptions.java
+++ src/main/java/com/factset/sdk/utils/authentication/RequestOptions.java
@@ -21,5 +21,5 @@ public class RequestOptions {
     SSLSocketFactory sslSocketFactory = HttpsURLConnection.getDefaultSSLSocketFactory();
 
     @Builder.Default
-    String userAgent = "fds-sdk/java/utils/1.1.3 (" + System.getProperty("os.name") + "; Java" + System.getProperty("java.version") + ")";
+    String userAgent = "fds-sdk/java/utils/1.1.4 (" + System.getProperty("os.name") + "; Java" + System.getProperty("java.version") + ")";
 }
